e1da0fdd13674fbf9193d408b196b4c0fd62d492
Dmitry Shuranov <dvshur@gmail.com>
2018-05-04 19:25:34 +0500

    runtime behavior can be changed with env variables

--- src/utils/getOptions.js
+++ src/utils/getOptions.js
@@ -1,6 +1,8 @@
 const YAML = require('yamljs');
 const path = require('path');
 
+const getEnvInt = varName => parseInt(process.env[varName]) || undefined;
+
 module.exports = () => {
   let config;
   try {
@@ -9,5 +11,12 @@ module.exports = () => {
     // eslint-disable-next-line
     console.error(err);
   }
-  return config;
+  return {
+    ...config,
+    blocksPerRequest:
+      getEnvInt('BLOCKS_PER_REQUEST') || config.blocksPerRequest,
+    blocksPerUpdate: getEnvInt('BLOCKS_PER_UPDATE') || config.blocksPerUpdate,
+    updateThrottleInterval:
+      getEnvInt('UPDATE_THROTTLE_INTERVAL') || config.updateThrottleInterval,
+  };
 };

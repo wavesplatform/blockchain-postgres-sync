8f3ae50fce052445d43013b2637c4c4cbbed8f0a
Dmitry Shuranov <dvshur@gmail.com>
2018-06-07 14:44:53 +0300

    add response sanitizing for u0000 symbol

--- src/api/requestBlocksBatch.js
+++ src/api/requestBlocksBatch.js
@@ -1,12 +1,16 @@
 const request = require('superagent');
 const jsonBigint = require('json-bigint')({ storeAsString: true });
 
-const bigIntParse = (res, fn) => {
+const bigIntParse = sanitize => (res, fn) => {
   res.text = '';
   res.setEncoding('utf8');
   res.on('data', chunk => (res.text += chunk));
-  res.on('end', err => fn(err, jsonBigint.parse(res.text)));
+  res.on('end', err => fn(err, jsonBigint.parse(sanitize(res.text))));
 };
+
+// \u0000 in JSON is problematic for PostgreSQL
+// removing it from strings
+const sanitize = text => text.replace(/\\u0000/g, '');
 
 const requestBlocksBatch = (start, options) =>
   request
@@ -17,7 +21,7 @@ const requestBlocksBatch = (start, options) =>
     )
     .retry(2)
     .buffer(true)
-    .parse(bigIntParse)
+    .parse(bigIntParse(sanitize))
     .then(r => r.body);
 
 module.exports = requestBlocksBatch;

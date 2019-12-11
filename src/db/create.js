20c3a9fadfb68fb0a2501d2542d2ac717ce530e3
Аleksej Petrov <apetrov@wavesplatform.com>
2019-12-11 15:50:57 +0300

    add pg statement timeout

--- src/db/create.js
+++ src/db/create.js
@@ -10,6 +10,7 @@ module.exports = options => {
       database: options.postgresDatabase,
       user: options.postgresUser,
       password: options.postgresPassword,
+      statement_timeout: options.postgresStatementTimeout
     });
   }
 

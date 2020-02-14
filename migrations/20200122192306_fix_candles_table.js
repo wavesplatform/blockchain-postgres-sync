01c6f978c644652585e66d6efcb5c35d98a0d88d
Аleksej Petrov <apetrov@wavesplatform.com>
2020-02-14 18:41:42 +0300

    cleaning

--- migrations/20200122192306_fix_candles_table.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-const upSqlFilePath = "./migrations/sql/20200122192306_fix_candles_table/up.sql";
-const downSqlFilePath = "./migrations/sql/20200122192306_fix_candles_table/down.sql";
-
-exports.up = knex => knex.schema.raw(fs.readFileSync(upSqlFilePath, "utf8"));
-
-exports.down = knex =>
-  knex.schema.raw(fs.readFileSync(downSqlFilePath, "utf8"));

e94c25c439b037484ded8361d5d1c47357d73482
Аleksej Petrov <apetrov@wavesplatform.com>
2020-07-09 11:07:37 +0300

    fix node requesting

--- src/api/requestHeight.js
+++ src/api/requestHeight.js
@@ -5,7 +5,7 @@ const requestHeight = options =>
   request
     .get(`${options.nodeAddress}/blocks/height`)
     .set('User-Agent', USER_AGENT)
-    .retry(2)
+    .retry(options.nodePollingRetriesCount, options.nodePollingRetriesDelay)
     .then(r => r.body.height);
 
 module.exports = requestHeight;

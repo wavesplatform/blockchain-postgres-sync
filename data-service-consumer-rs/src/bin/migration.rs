d0dff2cffcca04683a30074445f962062081ffff
Artyom Sidorenko <kronos43@mail.ru>
2023-03-03 14:05:34 +0300

    new migrator

--- data-service-consumer-rs/src/bin/migration.rs
+++ data-service-consumer-rs/src/bin/migration.rs
@@ -1,57 +1,75 @@
-use app_lib::config;
+use diesel::migration::Migration;
+use diesel::{migration, pg::PgConnection, Connection};
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 
-use diesel::{pg, Connection};
+use lib::{config, db::generate_postgres_url};
 
-use diesel_migrations::{
-    find_migrations_directory, revert_latest_migration_in_directory,
-    run_pending_migrations_in_directory,
-};
-use std::{convert::TryInto, env};
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
-enum Action {
-    Up,
-    Down,
+fn main() -> anyhow::Result<()> {
+    let action = action::parse_command_line()?;
+    let dbconfig = config::postgres::load()?;
+    let conn = PgConnection::establish(&generate_postgres_url(&dbconfig))?;
+    run(action, conn).map_err(|e| anyhow::anyhow!(e))
 }
 
-#[derive(Debug)]
-struct Error(&'static str);
-
-impl TryInto<Action> for String {
-    type Error = Error;
-
-    fn try_into(self) -> Result<Action, Self::Error> {
-        match &self[..] {
-            "up" => Ok(Action::Up),
-            "down" => Ok(Action::Down),
-            _ => Err(Error("cannot parse command line arg".into())),
+fn run(action: action::Action, mut conn: PgConnection) -> migration::Result<()> {
+    use action::Action::*;
+    match action {
+        ListPending => {
+            let list = conn.pending_migrations(MIGRATIONS)?;
+            if list.is_empty() {
+                println!("No pending migrations.");
+            }
+            for mig in list {
+                println!("Pending migration: {}", mig.name());
+            }
+        }
+        MigrateUp => {
+            let list = conn.run_pending_migrations(MIGRATIONS)?;
+            if list.is_empty() {
+                println!("No pending migrations.");
+            }
+            for mig in list {
+                println!("Applied migration: {}", mig);
+            }
+        }
+        MigrateDown => {
+            let mig = conn.revert_last_migration(MIGRATIONS)?;
+            println!("Reverted migration: {}", mig);
         }
     }
+    Ok(())
 }
 
-fn main() {
-    let action: Action = env::args().nth(1).unwrap().try_into().unwrap();
+mod action {
+    pub enum Action {
+        ListPending,
+        MigrateUp,
+        MigrateDown,
+    }
 
-    let config = config::load_migration_config().unwrap();
+    impl TryFrom<&str> for Action {
+        type Error = ();
 
-    let db_url = format!(
-        "postgres://{}:{}@{}:{}/{}",
-        config.postgres.user,
-        config.postgres.password,
-        config.postgres.host,
-        config.postgres.port,
-        config.postgres.database
-    );
-
-    let conn = pg::PgConnection::establish(&db_url).unwrap();
-    let dir = find_migrations_directory().unwrap();
-    let path = dir.as_path();
-
-    match action {
-        Action::Up => {
-            run_pending_migrations_in_directory(&conn, path, &mut std::io::stdout()).unwrap();
+        fn try_from(value: &str) -> Result<Self, Self::Error> {
+            match value {
+                "" | "list" => Ok(Action::ListPending),
+                "up" => Ok(Action::MigrateUp),
+                "down" => Ok(Action::MigrateDown),
+                _ => Err(()),
+            }
         }
-        Action::Down => {
-            revert_latest_migration_in_directory(&conn, path).unwrap();
-        }
-    };
+    }
+
+    pub fn parse_command_line() -> Result<Action, anyhow::Error> {
+        let action_str = std::env::args().nth(1).unwrap_or_default();
+        let action = action_str.as_str().try_into().map_err(|()| {
+            anyhow::anyhow!(
+                "unrecognized command line argument: {} (either 'up' or 'down' expected)",
+                action_str
+            )
+        })?;
+        Ok(action)
+    }
 }

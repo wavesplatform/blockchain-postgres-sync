ccb685998c546c047cac907c6dc1b74cbcdfe31e
Artyom Sidorenko <kronos43@mail.ru>
2023-03-22 10:44:38 +0300

    remove old files

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "$MIGRATE" == "true" ]
-then
-    npm run migrate -- --connection postgresql://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE
-fi
-
-node --max-old-space-size=2048 src/autorun/index.js

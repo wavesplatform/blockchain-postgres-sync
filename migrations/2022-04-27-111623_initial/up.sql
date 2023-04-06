0e86bd3ee4bd3876a3e3d16da506c2e7d4c67af5
Artyom Sidorenko <kronos43@mail.ru>
2023-04-06 10:56:36 +0300

    fix tickers view

--- migrations/2022-04-27-111623_initial/up.sql
+++ migrations/2022-04-27-111623_initial/up.sql
@@ -420,9 +420,7 @@ CREATE TABLE IF NOT EXISTS asset_tickers (
 CREATE OR REPLACE VIEW tickers(
     asset_id,
     ticker
-) AS SELECT DISTINCT ON (ticker) * FROM
-    (SELECT DISTINCT ON (asset_id) asset_id, ticker, uid FROM asset_tickers ORDER BY asset_id, uid DESC) as uord
-    ORDER BY ticker, uid DESC;
+) AS SELECT DISTINCT ON (asset_id) asset_id, ticker FROM asset_tickers ORDER BY asset_id, uid DESC;
 
 CREATE OR REPLACE VIEW assets(
     asset_id,
